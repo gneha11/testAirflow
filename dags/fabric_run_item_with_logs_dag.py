473cbbdf6b2df393054965f63db605a2e8a29c08
Neha Gupta <neha.gupta@regalrexnord.com>
2025-09-18 19:02:29 +0530

    Update fabric_run_item_with_logs_dag.py

--- dags/fabric_run_item_with_logs_dag.py
+++ dags/fabric_run_item_with_logs_dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from datetime import datetime
-from fabric_with_logs_operator import MSFabricRunItemLiveLogsOperator
+from fabric_with_logs_operator import MSFabricRunItemWithLiveLogsOperator
 
 with DAG(
     dag_id="fabric_run_item_with_logs_dag",
@@ -9,7 +9,7 @@ with DAG(
     schedule_interval=None,
 ) as dag:
 
-    run_with_logs = MSFabricRunItemLiveLogsOperator(
+    run_with_logs = MSFabricRunItemWithLiveLogsOperator(
         task_id="run_schema_and_folder_setup",
         fabric_conn_id="fabric-integration",
         workspace_id="bd883325-52e5-44d1-8742-72b4e6b3be82",
