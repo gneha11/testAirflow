13cb2d27be7bd688a875d5b224233d9f55741e95
Neha Gupta <neha.gupta@regalrexnord.com>
2025-09-18 19:06:54 +0530

    Update fabric_run_item_with_logs_dag.py

--- dags/fabric_run_item_with_logs_dag.py
+++ dags/fabric_run_item_with_logs_dag.py
@@ -2,13 +2,9 @@ from airflow import DAG
 from datetime import datetime
 from airflow.utils.dates import days_ago
 from fabric_with_logs_operator import MSFabricRunItemWithLiveLogsOperator
-from airflow.providers.microsoft.fabric.hooks.run_item import MSFabricRunItemHook
-
-# Initialize the hook
-fabric_hook = MSFabricRunItemHook(conn_id="fabric-integration")
 
 with DAG(
-    dag_id="fabric_run_item_with_live_logs_dag",
+    dag_id="fabric_run_item_with_logs_dag",
     start_date=days_ago(1),
     catchup=False,
     schedule_interval=None,
@@ -16,8 +12,9 @@ with DAG(
 
     run_with_logs = MSFabricRunItemWithLiveLogsOperator(
         task_id="run_schema_and_folder_setup",
-        hook=fabric_hook,  # pass hook object
+        fabric_conn_id="fabric-integration",  # pass conn_id, operator will init hook
         workspace_id="bd883325-52e5-44d1-8742-72b4e6b3be82",
         item_id="18023b08-26a5-44b5-9b8a-8cde87a2280f",
-        poll_interval=10,  # logs poll interval
+        poll_interval=10,
+        deferrable=True,  # allow async trigger for live logs
     )
