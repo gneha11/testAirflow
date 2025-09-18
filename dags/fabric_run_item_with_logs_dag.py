ec273cbd0edcb20856003dec4a15835cb7366f10
Neha Gupta <neha.gupta@regalrexnord.com>
2025-09-18 15:27:11 +0530

    Create fabric_run_item_with_logs_dag.py

---
+++ dags/fabric_run_item_with_logs_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from datetime import datetime
+from operators.fabric_with_logs_operator import MSFabricRunItemWithLogsOperator
+
+with DAG(
+    dag_id="fabric_run_item_with_logs_dag",
+    start_date=datetime(2025, 9, 18),
+    catchup=False,
+    schedule_interval=None,
+) as dag:
+
+    run_with_logs = MSFabricRunItemWithLogsOperator(
+        task_id="run_schema_and_folder_setup",
+        fabric_conn_id="fabric-integration",
+        workspace_id="bd883325-52e5-44d1-8742-72b4e6b3be82",
+        item_id="18023b08-26a5-44b5-9b8a-8cde87a2280f",
+        job_type="RunNotebook",
+        wait_for_termination=True,
+        deferrable=True,
+    )
