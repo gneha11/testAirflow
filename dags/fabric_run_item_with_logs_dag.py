ff1a2b8d79c5dce8a5f0c00e91cc9ba290930a27
Neha Gupta <neha.gupta@regalrexnord.com>
2025-09-18 19:14:21 +0530

    Update fabric_run_item_with_logs_dag.py

--- dags/fabric_run_item_with_logs_dag.py
+++ dags/fabric_run_item_with_logs_dag.py
@@ -1,11 +1,10 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.utils.dates import days_ago
 from fabric_with_logs_operator import MSFabricRunItemWithLiveLogsOperator
 
 with DAG(
-    dag_id="fabric_run_item_with_logs_dag",
-    start_date=days_ago(1),
+    dag_id="fabric_run_item_with_live_logs_dag",
+    start_date=datetime(2025, 9, 18),
     catchup=False,
     schedule_interval=None,
 ) as dag:
@@ -16,6 +15,6 @@ with DAG(
         workspace_id="bd883325-52e5-44d1-8742-72b4e6b3be82",
         item_id="18023b08-26a5-44b5-9b8a-8cde87a2280f",
         job_type="RunNotebook",
-        deferrable=True,        # live log streaming
         poll_interval=10,
+        deferrable=True,
     )
