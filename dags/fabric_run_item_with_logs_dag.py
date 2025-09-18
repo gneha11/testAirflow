58e2b6eef3264f3d1c75e181a61f68652585f11c
Neha Gupta <neha.gupta@regalrexnord.com>
2025-09-18 19:03:51 +0530

    Update fabric_run_item_with_logs_dag.py

--- dags/fabric_run_item_with_logs_dag.py
+++ dags/fabric_run_item_with_logs_dag.py
@@ -1,20 +1,23 @@
 from airflow import DAG
 from datetime import datetime
-from fabric_with_logs_operator import MSFabricRunItemWithLiveLogsOperator
+from airflow.utils.dates import days_ago
+from fabric_with_live_logs_operator import MSFabricRunItemWithLiveLogsOperator
+from airflow.providers.microsoft.fabric.hooks.run_item import MSFabricRunItemHook
+
+# Initialize the hook
+fabric_hook = MSFabricRunItemHook(conn_id="fabric-integration")
 
 with DAG(
-    dag_id="fabric_run_item_with_logs_dag",
-    start_date=datetime(2025, 9, 18),
+    dag_id="fabric_run_item_with_live_logs_dag",
+    start_date=days_ago(1),
     catchup=False,
     schedule_interval=None,
 ) as dag:
 
     run_with_logs = MSFabricRunItemWithLiveLogsOperator(
         task_id="run_schema_and_folder_setup",
-        fabric_conn_id="fabric-integration",
+        hook=fabric_hook,  # pass hook object
         workspace_id="bd883325-52e5-44d1-8742-72b4e6b3be82",
         item_id="18023b08-26a5-44b5-9b8a-8cde87a2280f",
-        job_type="RunNotebook",
-        wait_for_termination=True,
-        deferrable=False,
+        poll_interval=10,  # logs poll interval
     )
